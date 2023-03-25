d79d1fd517827c7c0985313fecf12c4bbcb5ad41
Daniel Harrington <novus@hotmail.com>
2023-03-25 19:12:06 -0400

    yup

--- website/response.py
+++ website/response.py
@@ -9,6 +9,25 @@ import os
 openai.api_key = os.environ.get("OPENAI_API_KEY")        # YOUR API KEY GOES HERE (replace)
 
 # In this function, you will accept a question from the user and generate a response using GPT
+def get_steps(userInput):
+    # take the input and phrase it as a question to chatgpt with a prompt to break it into steps with some marker for
+    # each step and return the list of steps as strings
+
+    try:
+        message = f'tell me how to {userInput} and break it down into numbered steps labelled by Step # marked by @@ at the beginning of the step. Start the beginning of the step with the action verb used in the step'
+        chatgpt_response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[{"role": "user", "content": message}],
+                temperature=0.1,
+                max_tokens=2000,
+                top_p=0.95)
+        chatgpt_response = chatgpt_response.split('@@');
+        chatgpt_response = [x for x in chatgpt_response if 'STEP' in x.upper()]
+        return chatgpt_response
+    except:
+        return ""
+
+
 def generate_dalle(instructionsList):
     '''Generates a list of images'''
     try:
@@ -31,20 +50,16 @@ def get_images(imagePrompts):
     '''
     Takes in a list of image prompt strings and creates an array of Dall-E Generated image URLS
     '''
-    urls = []
-    for imageprompt in imagePrompts:
-        image_object = openai.Image.create(
-                    prompt= imageprompt,
-                    n=1,
-                    size="512x512")
-
-        image_url = image_object['data'][0]['url']
-        urls.append(image_url)
-# In this function, you will generate an image based on the user prompt
-def generate_image(prompt):
     try:
-        # TODO: generate one image which relates to prompt, and return the url of the image as a string
-        return ""
+        urls = []
+        for imageprompt in imagePrompts:
+            image_object = openai.Image.create(
+                        prompt= imageprompt,
+                        n=1,
+                        size="512x512")
+
+            image_url = image_object['data'][0]['url']
+            urls.append(image_url)
+        return urls
     except:
-        # DO NOT DELETE!
         return ""
--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, redirect, render_template, request, flash, jsonify, url_for
-# from flask_login import login_required, current_user
+# from flask_login import login_required, current_user  
 # from .models import Note
 # from . import db
 # from sqlalchemy.sql import func
@@ -32,8 +32,10 @@ def generate_response():
     prompt = json.loads(request.data)
     promptText = prompt['text']
     print(promptText)
-    image_dict = {}
     steps =  get_steps(promptText)
+    print(steps)
     dallE_steps = generate_dalle(steps)
+    print(dallE_steps)
     image_links = get_images(dallE_steps)
+    print(image_links)
     return jsonify(dict(zip(steps,image_links)))
